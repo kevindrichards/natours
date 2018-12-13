0cc8c9240c9856cae6d93961cb2dd70e364572ba
Kevin Richards <kevin.d.richards@hotmail.com>
2018-12-13 18:38:41 +0000

    fix express routing for static html

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
-    res.render("index.html");
+    res.send("index.html");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Yelp Camp Server Has Started");
+    console.log("Server Has Started");
 });
